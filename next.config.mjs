7ba50f03fefd52d96d511c8ae355ed0c1b848de1
Angel Gabriel Gaitan <155489277+AngelGaitanG@users.noreply.github.com>
2024-07-04 15:07:54 -0300

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "lh3.googleusercontent.com"]
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
