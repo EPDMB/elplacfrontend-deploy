a4497059ce8e38a594a154b804b5cb6fd48af661
AngelGabrielG <ft49pfgrupo1@gmail.com>
2024-07-15 00:21:20 -0300

    Update ChooseRole.tsx

--- src/components/ChooseRole.tsx
+++ src/components/ChooseRole.tsx
@@ -2,15 +2,18 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { ChooseRoleProps, UniqueData } from "@/types";
-import { getUniqueData, putUser, putUserToSeller } from "@/helpers/services";
+import { getUniqueData, putUser, changeRole } from "@/helpers/services";
+import { useAuth } from "@/context/AuthProvider";
+
 
 const ChooseRole = ({ email, userId }: ChooseRoleProps) => {
+  const { token } = useAuth()
   console.log(email);
   const router = useRouter();
 
-  const handleRoleChange = async (userId: string, role: string) => {
+  const handleRoleChange = async (userId: string, role: string, token: string) => {
     try {
-      const response = await putUserToSeller(userId, { role: role });
+      const response = await changeRole(userId, role, token);
       console.log(response);
       router.push("/dashboard");
     } catch (error) {
@@ -20,16 +23,16 @@ const ChooseRole = ({ email, userId }: ChooseRoleProps) => {
 
   return (
     <div className="h-[40vh] w-screen flex flex-col justify-center items-center gap-5">
-      
+
       <div className="flex flex-row gap-5">
         <button
-          onClick={() => handleRoleChange(userId, "seller")}
+          onClick={() => handleRoleChange(userId, "seller", token)}
           className="text-primary-darker text-4xl font-bold"
         >
           ¿Querés vender?
         </button>
         <button
-          onClick={() => handleRoleChange(userId, "user")}
+          onClick={() => handleRoleChange(userId, "user", token)}
           className="text-secondary-darker text-4xl  font-bold"
         >
           ¿Querés comprar?
