f804b9dea1de031060020c6be1005f823780125e
AngelGabrielG <ft49pfgrupo1@gmail.com>
2024-07-18 01:50:15 -0300

    Update envs.ts

--- envs.ts
+++ envs.ts
@@ -3,4 +3,4 @@ dotenv.config();
 
 export const URL: string = process.env.API_URL || "https://myapp-backend-latest.onrender.com";
 export const MERCADOPAGO_PUBLIC_KEY: string =
-  process.env.MERCADOPAGO_PUBLIC_KEY || " ";
+  process.env.MERCADOPAGO_PUBLIC_KEY || "TEST-e211a22c-ca94-477e-9d15-4fce28331fa5";
