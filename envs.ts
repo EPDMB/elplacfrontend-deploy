d4ef74d10f23c8dce549493fd7d3f96aed32ac56
AngelGabrielG <ft49pfgrupo1@gmail.com>
2024-07-18 12:42:02 -0300

    Update envs.ts

--- envs.ts
+++ envs.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const URL: string = process.env.API_URL || "https://elplac-backend.onrender.com/";
+export const URL: string = process.env.API_URL || "https://elplac-backend.onrender.com";
 export const MERCADOPAGO_PUBLIC_KEY: string =
   process.env.MERCADOPAGO_PUBLIC_KEY || "TEST-e211a22c-ca94-477e-9d15-4fce28331fa5";
